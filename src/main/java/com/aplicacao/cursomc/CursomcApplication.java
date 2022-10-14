b79f5a998b3a4adecbf3fd69154f0a2f9490c6d2
Cezar Avila <cezaravila@hotmail.com>
2022-10-14 13:18:51 -0400

    Estado e ciade

--- src/main/java/com/aplicacao/cursomc/CursomcApplication.java
+++ src/main/java/com/aplicacao/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.aplicacao.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.aplicacao.cursomc.domain.Categoria;
+import com.aplicacao.cursomc.domain.Cidade;
+import com.aplicacao.cursomc.domain.Estado;
 import com.aplicacao.cursomc.domain.Produto;
 import com.aplicacao.cursomc.repositories.CategoriaRepository;
+import com.aplicacao.cursomc.repositories.CidadeRepository;
+import com.aplicacao.cursomc.repositories.EstadoRepository;
 import com.aplicacao.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,9 +22,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired 
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -38,13 +46,27 @@ public class CursomcApplication implements CommandLineRunner {
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p1.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
-		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+				
 	}
 
 }
--- src/main/java/com/aplicacao/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/aplicacao/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.aplicacao.cursomc.resources.exception;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
